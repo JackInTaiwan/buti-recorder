ff0d349fc48e5964a78279f9bc65f69e89d83267
jackintaiwan <b04901081@ntu.edu.tw>
2018-06-15 11:10:40 +0800

    [M] fix bugs in recoreder.py

--- butirecorder/recorder.py
+++ butirecorder/recorder.py
@@ -8,10 +8,11 @@ import torch as tor
 
 
 class Recorder :
-    def __init__(self, mode, recorder_name, save_path=None, models=None, info="") :
+    def __init__(self, mode, save_mode, save_path, recorder_name=None, models={}, info="") :
         self.id = uuid.uuid1().hex
         self.mode = mode
         self.recorder_name = recorder_name
+        self.save_mode = save_mode
         self.save_path = save_path
         self.models = models
         self.model_names = list(models.keys()) if len(models.keys()) > 0 else None
@@ -19,98 +20,105 @@ class Recorder :
         self.info = info
         self.data = dict()
 
-        self.step = 0
-        self.epoch = 0
+        self.steps = 0
+        self.epochs = 0
+
+        if not os.path.exists(os.path.join(self.save_path, "{}.json".format(self.recorder_name))) :
+            self.save_checkpoints()
 
 
     def __setattr__(self, key, value) :
         # checking types
         attr_types = {
+            "recorder_name": (str, type(None),),
             "mode": str,
-            "save_path": str,
-            "model_names": dict,
+            "save_path": (str, type(None),),
+            "models": (dict, type(None),),
         }
 
         for attr in attr_types :
             if key == attr and not isinstance(value, attr_types[attr]) :
-                raise TypeError("Recorder class attribute '{}' needs type {} but got {}.".format(attr_types[attr], type(value)))
+                raise TypeError("Recorder class parameter '{}' needs type {} but got {}.".format(key, attr_types[attr], type(value)))
 
         # checking opts
         if key == "mode" :
-            mode_opts = ["torch", "keras", "tf", ]
-            if value not in mode_opts :
-                raise ValueError("Recorder class attribute '{}' requires one of {}, but got '{}'"
-                                 .format(key, "/".join(mode_opts), value))
+            opts = ["torch", "keras", "tf", ]
+            if value not in opts :
+                raise ValueError("Recorder class parameter '{}' requires one of {}, but got '{}'"
+                                 .format(key, "/".join(opts), value))
+
+        if key == "save_mode" :
+            opts = ["state_dict", "model"]
+            if value not in opts :
+                raise ValueError("Recorder class parameter '{}' requires one of {}, but got '{}'"
+                                 .format(key, "/".join(opts), value))
 
         super().__setattr__(key, value)
 
 
-    def set(self, save_path, model_names) :
-        self.save_path = save_path
-        self.model_names = model_names
+    def set(self, models) :
+        self.models = models
+        self.model_names = list(models.keys()) if len(list(models.keys())) > 0 else None
 
 
     def load_recorder(self, recorder_fp) :
         with open(recorder_fp) as f :
             data = json.load(f)
         data = json.loads(data)
-
         self.id = data["id"]
+        self.recorder_name = data["recorder_name"]
         self.info = data["info"]
         self.mode = data["mode"]
         self.model_names = data["model_names"]
+        self.save_path = data["save_path"] if self.save_path else self.save_path
         self.data = data["data"]
-        self.step = data["step"]
-        self.epoch = data["epoch"]
+        self.steps = data["steps"]
+        self.epochs = data["epochs"]
 
-        split_index = 0
-        while recorder_fp.index("/", split_index) != split_index :
-            split_index = recorder_fp.index("/", split_index)
-
-        save_path = recorder_fp[:split_index] if split_index != 0 else "./"
-        recorder_name = recorder_fp[split_index:].strip(".json") if "/" in recorder_fp else recorder_fp.strip(".json")
-
-        self.save_path = save_path
-        self.recorder_name = recorder_name
+        self.save_checkpoints()
 
 
     def load_models(self) :
         import torch as tor
 
-        if len(self.model_names) > 0 :
-            for model_name in self.model_names :
-                model_fp = os.path.join(self.save_path, "{}.pkl".format(model_name))
-                model_state = tor.load(model_fp)
-                self.models[model_name] = model_state
+        if len(self.models) > 0 :
+            for name in self.models :
+                model_fp = os.path.join(self.save_path, "{}.pkl".format(name))
+                if self.save_mode == "state_dict" :
+                    model_state_dict = tor.load(model_fp)
+                    self.models[name].load_state_dict(model_state_dict)
 
         else :
-            raise ValueError("Recorder attribute 'model_name' length must > 0, but got 0.\
+            raise ValueError("Recorder parameter 'model_name' length must > 0, but got 0.\
              You may give a wrong json file, or you don't save the json file correctly.")
 
 
-    def load(self, recorder_fp, models) :
+    def load(self, recorder_fp) :
         self.load_recorder(recorder_fp)
-        self.load_models(models)
+        self.load_models()
 
         return self.models
 
 
     def checkpoint(self, data) :
-        step = self.step
+        steps = self.steps
+        print (self.data)
         for key in data :
             if key not in self.data :
-                self.data[key] = [(step, data[key])]
+                self.data[key] = [(steps, data[key])]
             else :
-                if not self.check_step_duplicate(step, data[key]) :
-                    self.data[key].append((step, data[key]))
+                if not self.check_step_duplicate(steps, self.data[key]) :
+                    self.data[key].append((steps, data[key]))
                 else :
-                    self.data[key][-1] = (step, data[key])
+                    self.data[key][-1] = (steps, data[key])
 
 
-    def check_step_duplicate(self, step, list) :
+    def check_step_duplicate(self, steps, list) :
+        print (list)
         step_list = [pair[0] for pair in list]
-        if step in step_list :
-            if len(step_list) > 1 and step_list.index(step) != len(step_list) -1 :
+        if steps in step_list :
+            print ("use!!!!!")
+            if len(step_list) > 1 and step_list.index(steps) != len(step_list) -1 :
                 raise ValueError("Method checkpoint requires no duplicate step as checkpoint.")
             else :
                 return True
@@ -120,45 +128,45 @@ class Recorder :
 
 
     def save_checkpoints(self) :
-        save_fp = os.path.join(self.save_path, self.recorder_name)
+        save_fp = "{}.json".format(os.path.join(self.save_path, self.recorder_name))
 
         output = dict()
         output["id"] = self.id
+        output["recorder_name"] = self.recorder_name
         output["save_path"] = self.save_path
         output["mode"] = self.mode
         output["model_names"] = self.model_names
         output["info"] = self.info
-        output["step"] = self.step
-        output["epoch"] = self.epoch
+        output["steps"] = self.steps
+        output["epochs"] = self.epochs
         output["data"] = self.data
-
         output_json = json.dumps(output)
 
         with open(save_fp, "w") as f :
             json.dump(output_json, f)
 
 
-    def save_parameters(self, parameters_dict) :
+    def save_state_dict(self) :
         if len(self.models) < 1 :
-            raise ValueError("Method save_models requires Recorder.model_names \
+            raise ValueError("Method save_state_dict requires Recorder.model_names \
             length > 0, but got {}.".format(len(self.models)))
 
         else :
-            if not isinstance(parameters_dict, dict) and len(self.model_names) != 1 :
-                raise ValueError("Method save_parameters requires that type of  \
-                parameters_dict is dict, or length of Recorder.model_names is 1.")
+            if not isinstance(self.models, dict) and len(self.model_names) != 1 :
+                raise ValueError("Method save_state_dict requires that type of  \
+                Recorder.models is dict, or length of Recorder.model_names is 1.")
 
             else :
-                for key in parameters_dict :
-                    model_save_fp = os.path.join(self.save_path, key)
-                    tor.save(parameters_dict[key], model_save_fp)
+                for key in self.models :
+                    model_save_fp = "{}.pkl".format(os.path.join(self.save_path, key))
+                    tor.save(self.models[key].state_dict(), model_save_fp)
+
+                self.save_mode = "state_dict"
 
 
     def step(self) :
-        self.step += 1
+        self.steps += 1
 
 
     def epoch(self) :
-        self.epoch += 1
-
-
+        self.epochs += 1
